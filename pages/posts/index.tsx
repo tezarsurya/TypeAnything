4c269fc6972c75491864fcbe930c5fb0e9864830
tezarsurya <59132336+tezarsurya@users.noreply.github.com>
2020-08-13 12:05:35 +0700

    add loading overlay

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,6 +1,8 @@
 import fetch from "isomorphic-unfetch";
 import { Card, CardContent, Typography, makeStyles } from "@material-ui/core";
 import { Layout } from "../../components/Layout";
+import { useState } from "react";
+import { ThreeHorseLoading } from "react-loadingg";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -9,31 +11,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const allPosts = (props) => {
+const allPosts = async () => {
   const classes = useStyles();
-  return (
-    <Layout>
-      {props.posts.map((post) => {
-        return (
-          <Card key={post._id} className={classes.card} elevation={3}>
-            <CardContent>
-              <Typography variant="h6" gutterBottom>
-                {post.title}
-              </Typography>
-              <Typography variant="body2">{post.content}</Typography>
-            </CardContent>
-          </Card>
-        );
-      })}
-    </Layout>
-  );
+  const [loading, setLoading] = useState(true);
+  const waitPosts = await fetch(`https://typeanything.herokuapp.com/api/posts`);
+  const posts = await waitPosts.json();
+  if (posts) {
+    setLoading(false);
+  } else if (loading) {
+    return <ThreeHorseLoading />;
+  } else {
+    return (
+      <Layout>
+        {posts.map((post) => {
+          return (
+            <Card key={post._id} className={classes.card} elevation={3}>
+              <CardContent>
+                <Typography variant="h6" gutterBottom>
+                  {post.title}
+                </Typography>
+                <Typography variant="body2">{post.content}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </Layout>
+    );
+  }
 };
 
-export async function getServerSideProps() {
-  const response = await fetch(`https://typeanything.herokuapp.com/api/posts`);
-  const posts = await response.json();
+// export async function getServerSideProps() {
+//   const response = await fetch(`https://typeanything.herokuapp.com/api/posts`);
+//   const posts = await response.json();
 
-  return { props: { posts: posts } };
-}
+//   return { props: { posts: posts } };
+// }
 
 export default allPosts;
